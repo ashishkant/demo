8a777076471a5f94a1c81bb958a6812477b53028
Ashish <31588456+ashishkant@users.noreply.github.com>
2026-06-23 18:32:32 +0000

    second

---
+++ django_project/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+# Create your views here.
+def home(request):
+    context = {'posts':posts,
+        'title': 'Zen of Python'
+    }
+    return render(request, 'blog/home.html',context)
+
+
+def about(request):
+    return render(request,'blog/about.html')
