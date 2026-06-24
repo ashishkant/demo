6deb85dadfc767e2415aa67b3451d862d0ff9b2c
Ashish <31588456+ashishkant@users.noreply.github.com>
2026-06-24 12:01:32 +0000

    third

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post , Employee
 
 
-posts = [
-    {
-        'title': 'Beautiful is better than ugly',
-        'author': 'John Doe',
-        'content': 'Beautiful is better than ugly',
-        'published_at': 'October 1, 2022'
-    },
-    {
-        'title': 'Explicit is better than implicit',
-        'author': 'Jane Doe',
-        'content': 'Explicit is better than implicit',
-        'published_at': 'October 1, 2022'
-    }
-]
+
 
 # Create your views here.
 def home(request):
+    posts=Post.objects.all()
+    employees=Employee.objects.all()
     context = {'posts':posts,
+        'employees':employees,
         'title': 'Zen of Python'
     }
     return render(request, 'blog/home.html',context)
