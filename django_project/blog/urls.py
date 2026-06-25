3bd443fd71c9a11b1703d7f46be899c053d7651e
Ashish <31588456+ashishkant@users.noreply.github.com>
2026-06-25 11:07:46 +0000

    fifth

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='posts'),
     path('post/create', views.create_post, name='post-create'),
+    path('post/edit/<int:id>/', views.edit_post, name='post-edit'),
     path('employee/create', views.create_employee, name='employee-create'),
     path('student/create', views.create_student, name='student-create'),
     path('about/',views.about, name='about'),
